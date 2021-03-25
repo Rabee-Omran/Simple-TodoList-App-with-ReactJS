b337da776b1d29b12b5fb46ae09a463e9ce65dd8
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-25 10:06:49 +0200

    Add Realtime Clock with lifeCycle Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Welcome from './components/Welcome';
 import List from './components/List';
+import LifeCycleExample from './components/LifeCycleExample';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
    <Welcome/>
    <List/>
+   <LifeCycleExample/>
     </div>
   );
 }
