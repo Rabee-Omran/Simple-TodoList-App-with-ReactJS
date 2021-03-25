15f8c102dac0b3322bd1dd49a715ab53789132bd
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-25 09:28:21 +0200

    initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Welcome from './components/Welcome';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,48 @@
+import react,{ Component } from 'react';
+
+
+class Welcome extends Component {
+
+    state= {
+        name :{
+            firstName:'Rabee',
+            lastName:'Omran'
+        }
+    }
+
+    firstNameHandler(event){
+       this.setState({
+           name :{
+               ...this.state.name,
+               firstName: event.target.value
+           }
+       })
+
+    }
+
+    lastNameHandler(event){
+        this.setState({
+            name :{
+                ...this.state.name,
+                lastName: event.target.value
+            }
+        })
+ 
+     }
+ 
+    render(){
+        return(
+            
+            <div>
+                <h1>{`${this.state.name.firstName} ${this.state.name.lastName}`}</h1>
+                <br/><span>First Name :</span> <input type='text' onChange={this.firstNameHandler.bind(this)} />
+                <br/><span>Last Name :</span> <input type='text'  onChange={this.lastNameHandler.bind(this)} />
+
+
+            </div>
+        );
+    }
+
+}
+
+export default Welcome;
